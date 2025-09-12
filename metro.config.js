324df8f0e1be43b6e6bad61939b23f9d50d235f6
fanthus <fanthus@163.com>
2025-09-12 19:49:25 +0800

    add deploy script

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// Add path alias support
+config.resolver.alias = {
+  '@': path.resolve(__dirname, './'),
+};
+
+module.exports = config;
