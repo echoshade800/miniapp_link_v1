ec477a083d23158f212336d82f1731a2d514614a
echoshade800 <echoshade800@gmail.com>
2025-09-12 19:17:36 +0800

    Added package-lock.json

--- app/game.js
+++ app/game.js
@@ -600,10 +600,6 @@ export default function Game() {
       }
     });
     setSelectedTiles([]);
-    
-    // Show invalid path tooltip
-    Alert.alert('Invalid Connection', 'Path cannot have more than 2 turns');
-    
     playSound('fail');
     vibrate();
     
