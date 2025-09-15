6c5a78d9c6d21915e41c2540e435d5782388dd09
fanthus <fanthus@163.com>
2025-09-15 15:52:01 +0800

    添加部署脚本

---
+++ metro.config.js
@@ -0,0 +1,19 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  resolver: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
