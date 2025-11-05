5e35cae6422cadc9b118766b3ea20f8641ddaed7
echoshade800 <echoshade800@gmail.com>
2025-11-05 18:03:42 +0800

    Updated game.js

--- app/game.js
+++ app/game.js
@@ -1552,7 +1552,7 @@ export default function Game() {
       return { tileSize: 48, emojiFontSize: 32, margin: 2, boardPadding: 12 };
     } else if (level <= 12) {
       // 6×10 棋盘，使用中等尺寸
-      return { tileSize: 32, emojiFontSize: 22, margin: 1.5, boardPadding: 10 };
+      return { tileSize: 40, emojiFontSize: 36, margin: 1.5, boardPadding: 11 };
     } else {
       // 8×10 棋盘，使用较小尺寸
       return { tileSize: 28, emojiFontSize: 18, margin: 1, boardPadding: 8 };
