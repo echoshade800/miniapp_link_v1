f9aa8bf5b02b589b7f00d5cac5dc346d6dd7cac8
echoshade800 <echoshade800@gmail.com>
2025-11-05 19:17:39 +0800

    Updated game.js

--- app/game.js
+++ app/game.js
@@ -2093,7 +2093,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 20,
     paddingVertical: 15,
-    marginTop: 60,
+    marginTop: 65,
   },
   pauseButton: {
     padding: 8,
