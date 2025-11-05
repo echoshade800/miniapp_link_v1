497364e344aaf61ba1e1991cb4169a94b24c8d43
echoshade800 <echoshade800@gmail.com>
2025-11-05 18:10:44 +0800

    Updated game.js

--- app/game.js
+++ app/game.js
@@ -1552,8 +1552,8 @@ export default function Game() {
       return { tileSize: 48, emojiFontSize: 32, margin: 2, boardPadding: 12 };
     } else if (level <= 12) {
       // 6×10 棋盘，使用中等尺寸
-      // 计算: (32 + 2*1.5) * 10 + 2*10 = 350 + 20 = 370px 宽度，适配大多数手机
-      return { tileSize: 32, emojiFontSize: 24, margin: 1.5, boardPadding: 10 };
+      // 计算: (34 + 2*1.5) * 10 + 2*10 = 370 + 20 = 390px 宽度
+      return { tileSize: 34, emojiFontSize: 26, margin: 1.5, boardPadding: 10 };
     } else {
       // 8×10 棋盘，使用较小尺寸
       return { tileSize: 28, emojiFontSize: 18, margin: 1, boardPadding: 8 };
